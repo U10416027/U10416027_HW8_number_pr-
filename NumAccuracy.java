9fbc2af46df491eb9f2c5f282a3009db475ac910
U10416027 <U10416027@go.utaipei.edu.tw>
2016-05-15 21:16:30 +0800

    Update NumAccuracy.java
    create constructor and the method to add the numbers

--- NumAccuracy.java
+++ NumAccuracy.java
@@ -1,13 +1,20 @@
 import java.math.BigDecimal;
 
 public class NumAccuracy {
-  private BigDecimal num1;
+  	private BigDecimal num1;
 	private BigDecimal num2;
 	
-	public NumAccuracy(){
-	
+	public NumAccuracy(double firstNum, double secondNum){
+		num1 = BigDecimal.valueOf(firstNum);  
+		num2 = BigDecimal.valueOf(secondNum);
 	}
-
+	
+	public  static  double  add(double firstNum, double secondNum){  
+		BigDecimal b1 = BigDecimal.valueOf(firstNum);  
+		BigDecimal b2 = BigDecimal.valueOf(secondNum); 
+	    	System.out.println("the answer is ;  " + b1.add(b2).doubleValue());
+        	return  b1.add(b2).doubleValue();
+    	}
 
 
 
