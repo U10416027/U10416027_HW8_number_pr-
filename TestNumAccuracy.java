887e0c24f388143ac0406656c14a3f305744065e
U10416027 <U10416027@go.utaipei.edu.tw>
2016-05-15 21:18:24 +0800

    Update TestNumAccuracy.java
    check the divScale is in the range or not, and determine the way to calculate numbers

--- TestNumAccuracy.java
+++ TestNumAccuracy.java
@@ -15,7 +15,22 @@ public class TestNumAccuracy {
 		System.out.println("set the scale when you want to devide those numbers");
 		divScale = input.nextInt();
 	
-	
+		while(divScale < 0){
+			System.out.println("the scale can't less than zero, enter again");
+			divScale = input.nextInt();
+		}
+		
+		System.out.println("which way you want to calculate ?");
+		System.out.println("1 = ' + '");
+		System.out.println("2 = ' - '");
+		System.out.println("3 = ' X '");
+		System.out.println("4 = ' / '");
+		int judge = input.nextInt();
+		
+		while(judge < 1 || judge > 4){
+			System.out.println("it's not in the range, enter again");
+			judge = input.nextInt();
+		}
 	
 	
 	
