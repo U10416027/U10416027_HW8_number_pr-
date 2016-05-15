3d40d38a2a050934c00b2ba49764ef399161dead
U10416027 <U10416027@go.utaipei.edu.tw>
2016-05-15 21:14:49 +0800

    Update TestNumAccuracy.java
    enter the two numbers and set the scale

--- TestNumAccuracy.java
+++ TestNumAccuracy.java
@@ -2,8 +2,18 @@ import java.util.*;
 
 public class TestNumAccuracy {
 	public static void main(String[] args) {
-	  double firstNum;
+	  	double firstNum;
 		double secondNum;
+		int divScale;
+		
+		Scanner input = new Scanner(System.in);
+		
+		System.out.println("enter 2 numbers you want to calculate");
+		firstNum = input.nextDouble();
+		secondNum = input.nextDouble();
+		
+		System.out.println("set the scale when you want to devide those numbers");
+		divScale = input.nextInt();
 	
 	
 	
