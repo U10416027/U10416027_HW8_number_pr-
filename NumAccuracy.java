73d38ab98edaeac310ba76077a44fdc880937442
U10416027 <U10416027@go.utaipei.edu.tw>
2016-05-15 21:20:47 +0800

    Update NumAccuracy.java
    create the method  sub to " - " the two numbers

--- NumAccuracy.java
+++ NumAccuracy.java
@@ -15,6 +15,13 @@ public class NumAccuracy {
 	    	System.out.println("the answer is ;  " + b1.add(b2).doubleValue());
         	return  b1.add(b2).doubleValue();
     	}
+    	
+    	public  static  double  sub(double firstNum, double secondNum){  
+		BigDecimal b1 = BigDecimal.valueOf(firstNum);  
+	    	BigDecimal b2 = BigDecimal.valueOf(secondNum); 
+	    	System.out.println("the answer is ;  " + b1.subtract(b2).doubleValue());
+        	return  b1.subtract(b2).doubleValue();
+    	}
 
 
 
